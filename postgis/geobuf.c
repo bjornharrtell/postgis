d63379bfc4f4b2b0382d95d5b2216e63d8183245
Björn Harrtell <bjorn.harrtell@sweco.se>
2016-07-14 15:51:19 +0200

    Try hardcoded data

--- postgis/geobuf.c
+++ postgis/geobuf.c
@@ -2,9 +2,20 @@
 
 void *geobuf_test(size_t* len) {
     void *buf;
+    uint32_t lengths = 1;
+    int64_t coord = 1;
     Data data = DATA__INIT;
+
+    Data__Geometry geometry = DATA__GEOMETRY__INIT;
+    geometry.type = DATA__GEOMETRY__TYPE__POINT;
+    geometry.n_lengths = 1;
+    geometry.lengths = &lengths;
+    geometry.n_coords = 1;
+    geometry.coords = &coord;
+    data.geometry = &geometry;
+
     *len = data__get_packed_size(&data);
-    buf = malloc(&len);
+    buf = malloc(*len);
     data__pack(&data, buf);
     return buf;
 }
